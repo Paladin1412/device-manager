18fefb597ea611bc315b6fee6ff64652ddf2f082
yaogang <yaogang@baidu.com>
2019-04-11 16:13:52 +0800

    Delete the unused code.
    Change-Id: I31804b509add0b08a23ced1ea38518e63273c7f3

--- src/main/java/com/baidu/iot/devicecloud/devicemanager/service/TtsService.java
+++ src/main/java/com/baidu/iot/devicecloud/devicemanager/service/TtsService.java
@@ -1,29 +1,21 @@
 package com.baidu.iot.devicecloud.devicemanager.service;
 
-import com.baidu.iot.devicecloud.devicemanager.bean.BaseResponse;
-import com.baidu.iot.devicecloud.devicemanager.bean.TlvMessage;
 import com.baidu.iot.devicecloud.devicemanager.client.http.ttsproxyclient.TtsProxyClient;
 import com.baidu.iot.devicecloud.devicemanager.client.http.ttsproxyclient.bean.TtsRequest;
 import com.baidu.iot.devicecloud.devicemanager.util.JsonUtil;
 import com.baidu.iot.devicecloud.devicemanager.util.PathUtil;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 /**
  * Created by Yao Gang (yaogang@baidu.com) on 2019/3/31.
