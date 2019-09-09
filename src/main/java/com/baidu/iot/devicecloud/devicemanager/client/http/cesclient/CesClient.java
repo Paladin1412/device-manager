92cd05e22d3c59070f01f2be290007431aa16d97
yaogang <yaogang@baidu.com>
2019-09-09 16:52:45 +0800

    duer4peppa-374 CES打点日志
    Change-Id: I3c39ca174c20f56747c0622bbf257298a4aea0e5

--- src/main/java/com/baidu/iot/devicecloud/devicemanager/client/http/cesclient/CesClient.java
+++ src/main/java/com/baidu/iot/devicecloud/devicemanager/client/http/cesclient/CesClient.java
@@ -1,8 +1,11 @@
 package com.baidu.iot.devicecloud.devicemanager.client.http.cesclient;
 
+import com.baidu.iot.devicecloud.devicemanager.cache.BnsCache;
 import com.baidu.iot.devicecloud.devicemanager.client.http.AbstractHttpClient;
 import com.baidu.iot.devicecloud.devicemanager.client.http.callback.CallbackFuture;
+import com.baidu.iot.devicecloud.devicemanager.util.PathUtil;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Preconditions;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -11,8 +14,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
+import javax.validation.constraints.NotNull;
+import java.net.InetSocketAddress;
 import java.util.concurrent.CompletableFuture;
+
+import static com.baidu.iot.devicecloud.devicemanager.constant.CommonConstant.SPLITTER_URL;
 
 /**
  * Created by Yao Gang (yaogang@baidu.com) on 2019/9/4.
@@ -22,12 +30,17 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 @Component
 public class CesClient extends AbstractHttpClient {
+    private static final String[] CES_LOG_PATH = {"saiya", "log"};
+
+    @Value("${ces.scheme:http://}")
+    private String cesScheme;
+
     @Value("${log.ces.url:}")
     private String cesUrl;
 
     public CompletableFuture<Response> sendCesLogAsync(JsonNode cesLog) {
         Request request = buildRequest(cesLog);
-        Assert.notNull(request, "The Redirecting Request is null");
+        Assert.notNull(request, "The CES Request is null");
         return sendAsyncWithFuture(request, new CallbackFuture());
     }
 
@@ -38,11 +51,46 @@ public class CesClient extends AbstractHttpClient {
             return null;
         }
 
+        String url = getFullPath();
+        if (StringUtils.isEmpty(url)) {
+            return null;
+        }
+
         Request.Builder builder = new Request.Builder()
-                .url(cesUrl)
+                .url(url)
                 .header(HttpHeaders.CONTENT_TYPE, org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
                 .post(requestBody);
 
         return builder.build();
+    }
+
+    private String getFullPath() {
+        String domainAddress = getDomainAddress();
+        if (StringUtils.startsWithIgnoreCase(domainAddress, cesScheme) ||
+                StringUtils.startsWithIgnoreCase(domainAddress, "bns://")) {
+            return domainAddress;
+        }
+        domainAddress = cesScheme + domainAddress;
+        return StringUtils.applyRelativePath(
+                PathUtil.lookAfterSuffix(domainAddress),
+                StringUtils.arrayToDelimitedString(CesClient.CES_LOG_PATH, SPLITTER_URL)
+        );
+    }
+
+    /**
+     * Resolve dcs address according to specified {@code message}
+     * @return {@code ip:port}
+     */
+    @NotNull
+    private String getDomainAddress() {
+        String domainAddress = null;
+        InetSocketAddress hashedAddress = BnsCache.getRandomCesAddress();
+        if (hashedAddress != null) {
+            domainAddress = PathUtil.dropOffPrefix(hashedAddress.toString(), SPLITTER_URL);
+        } else if (StringUtils.hasText(cesUrl)) {
+            domainAddress = cesUrl;
+        }
+        Preconditions.checkArgument(StringUtils.hasText(domainAddress), "Couldn't find any di address");
+        return domainAddress;
     }
 }
--- src/main/java/com/baidu/iot/devicecloud/devicemanager/service/handler/DuerLogHandler.java
+++ src/main/java/com/baidu/iot/devicecloud/devicemanager/service/handler/DuerLogHandler.java
@@ -13,15 +13,21 @@ import com.fasterxml.jackson.databind.node.IntNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.CompletableFuture;
+
+import static com.baidu.iot.devicecloud.devicemanager.constant.CoapConstant.COAP_RESPONSE_CODE_DUER_MSG_RSP_INVALID;
 import static com.baidu.iot.devicecloud.devicemanager.constant.CoapConstant.COAP_RESPONSE_CODE_DUER_MSG_RSP_NOT_FOUND;
 import static com.baidu.iot.devicecloud.devicemanager.constant.CoapConstant.COAP_RESPONSE_CODE_DUER_MSG_RSP_UNSUPPORTED_CONTENT_FORMAT;
 import static com.baidu.iot.devicecloud.devicemanager.constant.CoapConstant.COAP_RESPONSE_CODE_DUER_MSG_RSP_VALID;
 import static com.baidu.iot.devicecloud.devicemanager.constant.DataPointConstant.DATA_POINT_DUER_LOG;
+import static com.baidu.iot.devicecloud.devicemanager.util.HttpUtil.close;
 import static com.baidu.iot.devicecloud.devicemanager.util.HttpUtil.dataPointResponses;
 import static com.baidu.iot.devicecloud.devicemanager.util.HttpUtil.getDeviceInfoFromRedis;
 
@@ -68,43 +74,71 @@ public class DuerLogHandler extends AbstractLinkableDataPointHandler {
             return Mono.just(dataPointResponses(message, COAP_RESPONSE_CODE_DUER_MSG_RSP_NOT_FOUND,
                     String.format("Device may not online. id:%d cuid:%s", message.getId(), message.getDeviceId())));
         }
-        assemble(tree, deviceResource);
-        cesClient.sendCesLogAsync(tree);
-        return Mono.just(dataPointResponses(message, COAP_RESPONSE_CODE_DUER_MSG_RSP_VALID, null));
+        if (assemble(tree, deviceResource)) {
+            CompletableFuture<Response> future = cesClient.sendCesLogAsync(tree);
+            if (future != null) {
+                future.handleAsync(
+                        (r, t) -> {
+                            try(ResponseBody body = r.body()) {
+                                if (r.isSuccessful()) {
+                                    log.debug("Sending the ces log has succeeded. ces:{}", tree);
+                                } else {
+                                    log.debug("Sending the ces log has failed. ces:{} res:{}", tree, body == null ? null : body.string());
+                                }
+                            } catch (Exception e) {
+                                log.error("Sending the ces log has failed.", e);
+                            } finally {
+                                close(r);
+                            }
+                            return null;
+                        }
+                );
+            }
+            return Mono.just(dataPointResponses(message, COAP_RESPONSE_CODE_DUER_MSG_RSP_VALID, null));
+        }
+        return Mono.just(dataPointResponses(message, COAP_RESPONSE_CODE_DUER_MSG_RSP_INVALID, null));
     }
 
-    private void assemble(JsonNode tree, DeviceResource deviceResource) {
-        ObjectNode logObject = (ObjectNode) tree;
-        JsonNode dataNode = logObject.path("data");
-        if (dataNode.isArray()) {
-            ArrayNode dataArray = (ArrayNode) dataNode;
-            double la = deviceResource.getLatitude();
-            double lo = deviceResource.getLongitude();
-            for (JsonNode node : dataArray) {
-                ObjectNode on = (ObjectNode) node;
-                on.set("la", DoubleNode.valueOf(la));
-                on.set("lo", DoubleNode.valueOf(lo));
-                on.set("city", TextNode.valueOf(""));
-                on.set("net_type", TextNode.valueOf("1_0"));
-            }
+    private boolean assemble(JsonNode tree, DeviceResource deviceResource) {
+        if (tree == null || tree.isNull() || deviceResource == null) {
+            return false;
         }
-        logObject.set("device_id", TextNode.valueOf(deviceResource.getCuid()));
-        logObject.set("location_system", TextNode.valueOf(deviceResource.getGeoCoordinateSystem()));
-        DeviceStatus deviceStatus = deviceResource.getDeviceStatus();
-        if (deviceStatus != null) {
-            DeviceSystemInfo deviceSystemInfo = deviceStatus.getSystemInfo();
-            if (deviceSystemInfo != null) {
-                logObject.set("from", TextNode.valueOf(deviceSystemInfo.getFrom()));
-                logObject.set("client_id", TextNode.valueOf(deviceSystemInfo.getClientId()));
-                logObject.set("sdk_version", TextNode.valueOf(deviceSystemInfo.getSdkVersion()));
-                logObject.set("operation_system", TextNode.valueOf(deviceSystemInfo.getOperationSystem()));
-                logObject.set("operation_system_version", TextNode.valueOf(deviceSystemInfo.getOperationSystemVersion()));
-                logObject.set("device_brand", TextNode.valueOf(deviceSystemInfo.getDeviceBrand()));
-                logObject.set("device_model", TextNode.valueOf(deviceSystemInfo.getDeviceModel()));
-                logObject.set("ces_version", TextNode.valueOf(deviceSystemInfo.getCesVersion()));
-                logObject.set("abtest", IntNode.valueOf(deviceSystemInfo.getAbTest()));
-                logObject.set("real", IntNode.valueOf(deviceSystemInfo.getReal()));
+        try {
+            ObjectNode logObject = (ObjectNode) tree;
+            JsonNode dataNode = logObject.path("data");
+            if (dataNode.isArray()) {
+                ArrayNode dataArray = (ArrayNode) dataNode;
+                double la = deviceResource.getLatitude();
+                double lo = deviceResource.getLongitude();
+                for (JsonNode node : dataArray) {
+                    ObjectNode on = (ObjectNode) node;
+                    on.set("la", DoubleNode.valueOf(la));
+                    on.set("lo", DoubleNode.valueOf(lo));
+                    on.set("city", TextNode.valueOf(""));
+                    on.set("net_type", TextNode.valueOf("1_0"));
+                }
             }
+            logObject.set("device_id", TextNode.valueOf(deviceResource.getCuid()));
+            logObject.set("location_system", TextNode.valueOf(deviceResource.getGeoCoordinateSystem()));
+            DeviceStatus deviceStatus = deviceResource.getDeviceStatus();
+            if (deviceStatus != null) {
+                DeviceSystemInfo deviceSystemInfo = deviceStatus.getSystemInfo();
+                if (deviceSystemInfo != null) {
+                    logObject.set("from", TextNode.valueOf(deviceSystemInfo.getFrom()));
+                    logObject.set("client_id", TextNode.valueOf(deviceSystemInfo.getClientId()));
+                    logObject.set("sdk_version", TextNode.valueOf(deviceSystemInfo.getSdkVersion()));
+                    logObject.set("operation_system", TextNode.valueOf(deviceSystemInfo.getOperationSystem()));
+                    logObject.set("operation_system_version", TextNode.valueOf(deviceSystemInfo.getOperationSystemVersion()));
+                    logObject.set("device_brand", TextNode.valueOf(deviceSystemInfo.getDeviceBrand()));
+                    logObject.set("device_model", TextNode.valueOf(deviceSystemInfo.getDeviceModel()));
+                    logObject.set("ces_version", TextNode.valueOf(deviceSystemInfo.getCesVersion()));
+                    logObject.set("abtest", IntNode.valueOf(deviceSystemInfo.getAbTest()));
+                    logObject.set("real", IntNode.valueOf(deviceSystemInfo.getReal()));
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 }
